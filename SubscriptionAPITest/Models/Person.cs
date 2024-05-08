abfc68f537bb820b73b19ae0c445cddbddd2a559
sebiflorinp <sebiflorinp@gmail.com>
2024-05-08 15:42:35 +0300

    ADD Person and Subscription Models

---
+++ SubscriptionAPITest/Models/Person.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SubscriptionAPITest.Models;
+
+public class Person
+{
+    [Key]
+    public int PersonId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Username { get; set; } = null!;
+
+    [EmailAddress] public string Email { get; set; } = null!;
+    
+    [Range(1, 100)]
+    public int Age { get; set; }
+    
+    // Navigation
+    public ICollection<Subscription> Subscriptions { get; set; }
+}
---
+++ SubscriptionAPITest/Models/Subscription.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SubscriptionAPITest.Models;
+
+public class Subscription
+{
+    [Key]
+    public int SubscriptionId { get; set; }
+    
+    public string? Name { get; set; }
+    
+    [Range(0, 9999.99)]
+    public decimal Price { get; set; }
+
+    public DateTime SubscribedOn { get; set; } = DateTime.Now;
+    
+    // Navigation
+    public int PersonId { get; set; }
+    
+    public ICollection<Person> Persons { get; set; }
+}
